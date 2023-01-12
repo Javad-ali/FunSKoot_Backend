8accde2ac53466faf6c82942a08475b75e431f4e
JavadAli <javadalivpk45@gmail.com>
2023-01-12 17:54:26 +0530

    add cors

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import ChatRoutes from "./routes/chat.js";
 import MessageRoutes from "./routes/message.js";
-import {register} from "./controllers/auth.js";
-import {createPost} from "./controllers/posts.js"; 
-import {verifyToken} from "./middleware/auth.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
-import { users,posts } from "./data/index.js";
+import { users, posts } from "./data/index.js";
 import adminRoute from './routes/admin.js'
 
 
-import {createServer} from 'http';
-import {Server} from 'socket.io';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,22 +35,23 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("tiny"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({origin:'http://localhost:3000'}));
+app.use(cors({ origin: ['http://localhost:3000', "https://www.funskoot.online/"] }))
+
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 
 
 /*ROUTES WITH FILES*/
-app.post("/auth/register",register);
-app.post("/posts",verifyToken, createPost);
+app.post("/auth/register", register);
+app.post("/posts", verifyToken, createPost);
 
 /* ROUTES */
-app.use("/auth",authRoutes);
-app.use("/users",userRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
-app.use("/chat",ChatRoutes);
-app.use("/message",MessageRoutes);
-app.use('/admin',adminRoute)
+app.use("/chat", ChatRoutes);
+app.use("/message", MessageRoutes);
+app.use('/admin', adminRoute)
 
 /* MONGOSE SETUP*/
 const PORT = process.env.PORT || 6001;
@@ -58,60 +59,59 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => {
-    console.log("DATABASE CONNECTED");
-    /* ADD DATA ONE TIME */
-// User.insertMany(users);
-// Post.insertMany(posts);
-})
-.catch((error) => console.log(`${error} did not connected`));
+    .then(() => {
+        console.log("DATABASE CONNECTED");
+        /* ADD DATA ONE TIME */
+        // User.insertMany(users);
+        // Post.insertMany(posts);
+    })
+    .catch((error) => console.log(`${error} did not connected`));
 
 
-const httpServer=createServer(app);
+const httpServer = createServer(app);
 
-const io=new Server(httpServer,{
-    cors:{
-        origin:'http://localhost:3000'
+const io = new Server(httpServer, {
+    cors: {
+        origin: ['http://localhost:3000', "https://www.funskoot.online/"]
     }
 })
 
-let activeUsers =[]
+let activeUsers = []
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
 
     // add new User
-    socket.on('new-user-add',(newUserId)=>{
+    socket.on('new-user-add', (newUserId) => {
         // if user is not added previously
-        if(!activeUsers.some((user)=>user.userId === newUserId))
-        {
-             activeUsers.push({
-                userId:newUserId,
+        if (!activeUsers.some((user) => user.userId === newUserId)) {
+            activeUsers.push({
+                userId: newUserId,
                 socketId: socket.id
-             })
+            })
         }
-         console.log("Connected Users", activeUsers);
+        console.log("Connected Users", activeUsers);
         io.emit('get-users', activeUsers)
     })
 
     //send message
-    socket.on("send-message",(data)=>{
-        const {receiverId}= data;
-        const user = activeUsers.find((user)=>user.userId === receiverId)
-        console.log("Sending from socket to :",receiverId);
-        console.log("Data",data);
-        if(user){
-            io.to(user.socketId).emit("receive-message",data)
+    socket.on("send-message", (data) => {
+        const { receiverId } = data;
+        const user = activeUsers.find((user) => user.userId === receiverId)
+        console.log("Sending from socket to :", receiverId);
+        console.log("Data", data);
+        if (user) {
+            io.to(user.socketId).emit("receive-message", data)
         }
     })
 
-    socket.on("disconnect",()=>{
-        activeUsers =activeUsers.filter((user)=>user.socketId !==socket.id);
+    socket.on("disconnect", () => {
+        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
         console.log("User Disconnected", activeUsers);
         io.emit('get-users', activeUsers)
 
     })
 })
 
-httpServer.listen(process.env.PORT,()=>
+httpServer.listen(process.env.PORT, () =>
     console.log(`Connected on ${process.env.PORT}`)
 )
